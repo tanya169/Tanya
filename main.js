3e0f55b5dfbf2816493401e7a7a9e3a4966d9912
tanya169 <72605062+tanya169@users.noreply.github.com>
2020-12-04 22:26:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function setDate() {
-    document.getElementById("date").innerHTML = new Date().toDateString();
+function getParagraph1()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
+        inputs.join(". ");
+    }
+    document.getElementById("showParagraph1").innerHTML = inputs.join(". ");   
 }
+function getParagraph2()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("para2_input_box_" + i).value);
+        inputs.join(". ");
+    }
+    document.getElementById("showParagraph2").innerHTML = inputs.join(". ");  
+}
+
