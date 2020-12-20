88601b596190c818432f58d6cd315bc8bf1afa03
tanya169 <72605062+tanya169@users.noreply.github.com>
2020-12-20 18:18:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,81 @@
-menu_list_array = ["Chicken Tandoori Pizza",
-    "Veg Supreme Pizza", "Paneer Tika Pizza", "Deluxe Veggie Pizza", "Veg Extravaganza Pizza"
-];
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-function getmenu() {
-    var htmldata;
-    htmldata = "<ol class='menulist'>"
-    menu_list_array.sort();
-    for (var i = 0; i < menu_list_array.length; i++) {
-        htmldata = htmldata + '<li>' + menu_list_array[i] + '</li>'
-    }
-    htmldata = htmldata + "</ol>"
-    document.getElementById("display_menu").innerHTML = htmldata;
+car1_width = 120;
+car1_height = 70;
+car1_image = "https://png.pngitem.com/pimgs/s/150-1509028_cars-plan-view-png-top-view-car-png.png";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "https://www.clipartmax.com/png/middle/17-172157_racing-car-cartoon-car-top-view.png";
+car2_x = 10;
+car2_y = 100;
+
+background_image = "https://t4.ftcdn.net/jpg/02/98/60/67/360_F_298606783_ZH5CHzsFASBM1m5gLAsviFvpsIpj2IRS.jpg";
+
+function add() {
+    car1_imgTag = new Image();
+    car1_imgTag.onload = uploadcar1;
+    car1_imgTag.src = car1_image;
+
+    car2_imgTag = new Image();
+    car2_imgTag.onload = uploadcar2;
+    car2_imgTag.src = car2_image;
+    
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = background_image;
 }
 
-function add_item(){
-    var htmldata;
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-    menu_list_array.sort();
-    htmldata="<section class='cards'>"
-    for (var i = 0; i < menu_list_array.length; i++) {
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
 
-        htmldata= htmldata+ '<div class="card">' + '<img src="https://image.freepik.com/free-photo/mixed-ingredient-pizza-with-cheese-tomatoes_114579-3585.jpg"/>' + menu_list_array[i] + '</div>'
+function uploadcar1() {
+    ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+}
+
+function uploadcar2() {
+    ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if (keyPressed == '38') {
+        car1_up();
+        console.log("up arrow key");
     }
-    htmldata= htmldata+ "</section>"
-    document.getElementById("display_addedmenu").innerHTML = htmldata;
+    if (keyPressed == '40') {
+        car1_down();
+        console.log("down arrow key");
+    }
+    if (keyPressed == '37') {
+        car1_left();
+        console.log("left arrow key");
+    }
+    if (keyPressed == '39') {
+        car1_right();
+        console.log("right arrow key");
+    }
+    if (keyPressed == '87') {
+        car2_up();
+        console.log("key w");
+    }
+    if (keyPressed == '83') {
+        car2_down();
+        console.log("key s");
+    }
+    if (keyPressed == '65') {
+        car2_left();
+        console.log("key a");
+    }
+    if (keyPressed == '68') {
+        car2_right();
+        console.log("key d");
+    }
 }
