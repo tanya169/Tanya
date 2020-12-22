ea298e65415d72fc410eaabc346e3f5e9596ce93
tanya169 <72605062+tanya169@users.noreply.github.com>
2020-12-22 21:26:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,165 +1,39 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
-gamestatus = document.getElementById('game_status')
-car1_width = 120;
-car1_height = 70;
-car1_image = "https://png.pngitem.com/pimgs/s/150-1509028_cars-plan-view-png-top-view-car-png.png";
-car1_x = 10;
-car1_y = 10;
+var canvas = new fabric.Canvas('myCanvas');
 
-car2_width = 120;
-car2_height = 70;
-car2_image = "https://www.clipartmax.com/png/middle/17-172157_racing-car-cartoon-car-top-view.png";
-car2_x = 10;
-car2_y = 100;
+player_x = 10;
+player_y = 10;
 
-background_image = "https://t4.ftcdn.net/jpg/02/98/60/67/360_F_298606783_ZH5CHzsFASBM1m5gLAsviFvpsIpj2IRS.jpg";
+block_image_width = 30;
+block_image_height = 30;
 
-function add() {
-    car1_imgTag = new Image();
-    car1_imgTag.onload = uploadcar1;
-    car1_imgTag.src = car1_image;
+var player_object= "";
+var block_image_object= "";
 
-    car2_imgTag = new Image();
-    car2_imgTag.onload = uploadcar2;
-    car2_imgTag.src = car2_image;
-
-    background_imgTag = new Image();
-    background_imgTag.onload = uploadBackground;
-    background_imgTag.src = background_image;
+function player_update()
+{
+    fabric.Image.fromURL("https://i.dlpng.com/static/png/1747685-png-captain-america-captian-america-png-1200_1855_preview.png", function(Img) {
+    player_object = Img;
+    
+    player_object.scaleToWidth(150);
+    player_object.scaleToHeight(140);
+    player_object.set({
+    top:player_y,
+    left:player_x
+    });
+canvas.add(player_object);
+});
 }
-
-function uploadBackground() {
-    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
-}
-
-function uploadcar1() {
-    ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
-}
-
-function uploadcar2() {
-    ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
-}
-
-window.addEventListener("keydown", my_keydown);
-
-function my_keydown(e) {
-    keyPressed = e.keyCode;
-    console.log(keyPressed);
-    if (keyPressed == '38') {
-        car1_up();
-        console.log("up arrow key");
-    }
-    if (keyPressed == '40') {
-        car1_down();
-        console.log("down arrow key");
-    }
-    if (keyPressed == '37') {
-        car1_left();
-        console.log("left arrow key");
-    }
-    if (keyPressed == '39') {
-        car1_right();
-        console.log("right arrow key");
-    }
-    if (keyPressed == '87') {
-        car2_up();
-        console.log("key w");
-    }
-    if (keyPressed == '83') {
-        car2_down();
-        console.log("key s");
-    }
-    if (keyPressed == '65') {
-        car2_left();
-        console.log("key a");
-    }
-    if (keyPressed == '68') {
-        car2_right();
-        console.log("key d");
-    }
-    if (car1_x > 700) {
-        console.log("car1 Won");
-        document.getElementById('game_status').innerHTML = "Car 1 Won!!";
-    }
-}
-
-function car1_up() {
-    if (car1_y >= 0) {
-        car1_y = car1_y - 10;
-        console.log("When up arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car1_down() {
-    if (car1_y <= 500) {
-        car1_y = car1_y + 10;
-        console.log("When down arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car1_left() {
-    if (car1_x >= 0) {
-        car1_x = car1_x - 10;
-        console.log("When left arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car1_right() {
-    if (car1_x <= 700) {
-        car1_x = car1_x + 10;
-        console.log("When right arrow is pressed, x = " + car1_x + " | y = " + car1_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car2_up() {
-    if (car2_y >= 0) {
-        car2_y = car2_y - 10;
-        console.log("When w is pressed, x = " + car2_x + " | y = " + car2_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car2_down() {
-    if (car2_y <= 500) {
-        car2_y = car2_y + 10;
-        console.log("When a is pressed, x = " + car2_x + " | y = " + car2_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car2_left() {
-    if (car2_x >= 0) {
-        car2_x = car2_x - 10;
-        console.log("When s is pressed, x = " + car2_x + " | y = " + car2_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
-}
-
-function car2_right() {
-    if (car2_x <= 700) {
-        car2_x = car2_x + 10;
-        console.log("When d is pressed, x = " + car2_x + " | y = " + car2_y);
-        uploadBackground();
-        uploadcar1();
-        uploadcar2();
-    }
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img) {
+        player_object = Img;
+        
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+        top:player_y,
+        left:player_x
+        });
+    canvas.add(block_image_object);
+    });  
 }
