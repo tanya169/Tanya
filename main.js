a0ce8fb950e84a7b5cb94729fda3a1fc089efad5
tanya169 <72605062+tanya169@users.noreply.github.com>
2021-02-21 12:07:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,135 +1,33 @@
-var canvas = new fabric.Canvas('myCanvas');
-
-player_x = 10;
-player_y = 10;
-
-block_image_width = 30;
-block_image_height = 30;
-
-var player_object= "";
-var block_image_object= "";
-
-function player_update()
-{
-    fabric.Image.fromURL("player.png", function(Img) {
-    player_object = Img;
-    
-    player_object.scaleToWidth(150);
-    player_object.scaleToHeight(140);
-    player_object.set({
-    top:player_y,
-    left:player_x
-    });
-canvas.add(player_object);
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
 });
-}
-function new_image(get_image)
+camera = document.getElementById("camera");
+Webcam.attach( '#camera' );
+function take_snapshot()
 {
-    fabric.Image.fromURL(get_image, function(Img) {
-        block_image_object = Img;
-        
-        block_image_object.scaleToWidth(block_image_width);
-        block_image_object.scaleToHeight(block_image_height);
-        block_image_object.set({
-        top:player_y,
-        left:player_x
-        });
-    canvas.add(block_image_object);
-    });  
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id"captured_image" src="'+data_uri+'"/>';
+    });
 }
-window.addEventListener("keydown", my_keydown);
-
-function my_keydown(e) {
-    keyPressed = e.keyCode;
-    console.log(keyPressed);
-    if (e.shiftKey == true && keyPressed == '80') {
-        console.log("p and shift pressed together");
-        block_image_width = block_image_width + 10;
-        block_image_height = block_image_height + 10;
-        document.getElementById("current_width").innerHTML = block_image_width;
-        document.getElementById("current_height").innerHTML = block_image_height;
-    }
-    if (e.shiftKey && keyPressed == '77') {
-        console.log("m and shift pressed together");
-        block_image_width = block_image_width - 10;
-        block_image_height = block_image_height - 10;
-        document.getElementById("current_width").innerHTML = block_image_width;
-        document.getElementById("current_height").innerHTML = block_image_height;
-    }
-    if (keyPressed == '70') {
-        new_image('ironman_face.png');
-        console.log("f");
-    }
-    if (keyPressed == '66') {
-        new_image('spiderman_body.png');
-        console.log("b");
-    }
-    if (keyPressed == '76') {
-        new_image('hulk_legs.png');
-        console.log("l");
-    }
-    if (keyPressed == '82') {
-        new_image('thor_right_hand.png');
-        console.log("r");
-    }
-    if (keyPressed == '72') {
-        new_image('captain_america_left_hand.png');
-        console.log("h");
-    }
-    if (keyPressed == '38') {
-        up();
-        console.log("up");
-    }
-    if (keyPressed == '40') {
-        down();
-        console.log("down");
-    }
-    if (keyPressed == '37') {
-        left();
-        console.log("left");
-    }
-    if (keyPressed == '39') {
-        right();
-        console.log("right");
-    }
+console.log('m15 version:', m15.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hIfvOvE67/model.json',modelLoaded);
+function modelLoaded() {
+    console.log('Model Loaded!');
 }
-
-function up() {
-    if (player_y >= 0) {
-        player_y = player_y - block_image_height;
-        console.log("block image height = " + block_image_height);
-        console.log("When Up arrow key is pressed, X = " + player_x + " , Y = " + player_y);
-        canvas.remove(player_object);
-        player_update();
-    }
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
 }
-
-function down() {
-    if (player_y <= 500) {
-        player_y = player_y + block_image_height;
-        console.log("block image height = " + block_image_height);
-        console.log("When Down arrow key is pressed, X = " + player_x + " , Y = " + player_y);
-        canvas.remove(player_object);
-        player_update();
-    }
-}
-
-function left() {
-    if (player_x > 0) {
-        player_x = player_x - block_image_width;
-        console.log("block image width = " + block_image_width);
-        console.log("When Left arrow key is pressed, X = " + player_x + " , Y = " + player_y);
-        canvas.remove(player_object);
-        player_update();
-    }
-}
-
-function right() {
-    if (player_x <= 850) {
-        player_x = player_x + block_image_width;
-        console.log("block image width = " + block_image_width);
-        console.log("When Right arrow key is pressed, X = " + player_x + " , Y = " + player_y);
-        canvas.remove(player_object);
-        player_update();
-    }
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+      } else {
+        console.log(results);
+        document.getElementById("result_person_name").innerHTML = results[0].label;
+        document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
+      }
 }
