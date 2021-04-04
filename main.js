e8c0c9da80c2c7610c1fe22b99905b17ef95ed1d
tanya169 <72605062+tanya169@users.noreply.github.com>
2021-04-04 13:05:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,26 @@
-noseX=0;
-noseY=0;
-
-function preload() {
-  clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
-}
-
 function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.size(300, 300);
-  video.hide();
-
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded() {
-  console.log('PoseNet Is Initialized');
-}
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-40;
-    noseY = results[0].pose.nose.y;
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uYRlbcyeN/',modelLoaded);
   }
-}
-
-function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clown_nose, noseX, noseY, 85, 40);
-}
-
-function take_snapshot(){    
-  save('myFilterImage.png');
-}
-
-
+  
+  function modelLoaded() {
+      console.log('Model Loaded!');
+  }
+  
+  function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_person_name").innerHTML = results[0].label;
+      document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+  }
