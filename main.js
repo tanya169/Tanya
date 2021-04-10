331a5a801e6c4ab3c6061adc236c499b308b9550
tanya169 <72605062+tanya169@users.noreply.github.com>
2021-04-10 21:16:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uYRlbcyeN/',modelLoaded);
-  }
-  
-  function modelLoaded() {
-      console.log('Model Loaded!');
-  }
-  
-  function draw() {
-    image(video, 0, 0, 300, 300);
-    classifier.classify(video, gotResult);
-  }
-  
-  function gotResult(error, results) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(results);
-      document.getElementById("result_person_name").innerHTML = results[0].label;
-      document.getElementById("result_person_accuracy").innerHTML = results[0].confidence.toFixed(3);
-    }
-  }
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
