1380fdc607a1b4c0437c4edd5f3c79b1bf45b345
tanya169 <72605062+tanya169@users.noreply.github.com>
2021-04-15 14:00:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ leftWristY = 0;
 
 function preload()
 {
-	song1 = loadSound("Harry_Potter_Theme-550179.mp3");
-	song2 = loadSound("");
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
 }
 
 function setup() {
@@ -52,4 +52,48 @@ function gotPoses(results)
 	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
 		
   }
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#ADD8E6");
+	stroke("#FF5070");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+		}
+	}
+
+}
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
